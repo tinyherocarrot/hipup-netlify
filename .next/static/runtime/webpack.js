9d2959ee1492e6a234da7df9eef165c3ab7743d7
Andrew Huang <andrw.huang@gmail.com>
2019-04-29 16:50:26 -0700

    testing new static next config

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fe54212e7f587886fce6";
+/******/ 	var hotCurrentHash = "7cf9f09a689fd818bbed";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- next.config.js
+++ next.config.js
@@ -7,5 +7,14 @@ module.exports = {
             }
         )
         return cfg;
+    },
+    exportPathMap: async (defaultPathMap) => {
+        return {
+            '/': { page: '/' },
+            '/contact': { page: '/contact' },
+            '/p/hello-nextjs': { page: '/post', query: { title: 'Hello Next.js' } },
+            '/p/learn-nextjs': { page: '/post', query: { title: 'Learn Next.js is awesome' } },
+            '/p/deploy-nextjs': { page: '/post', query: { title: 'Deploy apps with Zeit' } },
+        }
     }
 }
